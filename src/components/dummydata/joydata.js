12daa3a16758adf031d6c62d39b8fd3076be72a7
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-17 10:55:06 +0300

    Add dummy data

---
+++ src/components/dummydata/joydata.js
@@ -0,0 +1,38 @@
+const yatches = [
+  {
+    id: 1,
+    name: "yatch house one",
+    description: "It has a nice view of the ocean with a private beach",
+    image:
+      "https://www.houseboating.org/media/new/Dale-Hollow-Lake-Houseboat-Rentals-Media-25-800x600.jpg",
+  },
+  {
+    id: 2,
+    name: "yatch house two",
+    description: "Family vacation house boat",
+    image: "http://www.no1-houseboat.com/uploads/pics/web2_03.jpg",
+  },
+  {
+    id: 3,
+    name: "yatch house three",
+    description: "Fun experience boat house",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9qi3OnBhhdP62Mktg3uc2jmjBxJ2dBFAzVyF3Y7GeZqhHij7Xkzui7ugXCG7wJh32hkY&usqp=CAU",
+  },
+  {
+    id: 4,
+    name: "yatch house four",
+    description: "Romantic experience houseboat",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBvFFDA-ZaQi-QZPI58Ccqeq9HEqsmYJ2IIWtL84wq2NXYt2QvJmNPKKexurOqFfif_rU&usqp=CAU",
+  },
+  {
+    id: 5,
+    name: "yatch house five",
+    description: "Summer house boat",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBlVtu9_K6FmOq7nBj4KnPs9J8bBTF9mE_mFDqdciVtlmaIxVIvrqwUXSkASdp964k9Lo&usqp=CAU",
+  },
+];
+
+export default yatches;
