59e286d2bdf26bedef29752a491171eb6e12eb09
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-19 11:50:33 +0300

    Add nect icon

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import SearchIcon from "@mui/icons-material/Search";
+import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
 import './homepage.css';
+import { border } from '@mui/system';
+
 
 const HomePage = () => (
   <div>
@@ -21,19 +24,25 @@ const HomePage = () => (
         <div className='wallpaper-content'>
         <SearchIcon
           sx={{
-            color: "#00B5E2",
-            borderRadius: "45%",
+            color: "#1663be",
+            borderRadius: "48%",
             border: "1px solid white",
             backgroundColor: "white",
             fontSize: "1rem",
-            width: "4rem",
-            height: "2.5rem",
+            width: "2.6rem",
+            height: "4rem",
             position: "absolute",
             top:"20px",
             right: "2rem",
+            transform:"rotate(90deg)",
           }}
         />
         <h1> Rent a Yatch House with Yatch flex</h1>
+        </div>
+        <div className="reserve-button">
+            <button className='reserve'>Reserve Now
+            <ExpandCircleDownIcon sx={{color:"#fff", transform:"rotate(-90deg)"}} />
+            </button>
         </div>
     </div>
   </div>
