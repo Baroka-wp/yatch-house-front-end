43e667724e13989a732f7263c4942792f928700f
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-19 08:04:21 +0300

    Add search icon and style headings

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import SearchIcon from "@mui/icons-material/Search";
+import './homepage.css';
 
-const HomePage = () => {
-    return (
-        <div>
-        <h1>Home Page</h1>
-        </div>
-    );
-}
+const HomePage = () => (
+  <div>
+    <div
+      className='wallpaper'
+      style={{
+        backgroundImage: "url(/images/maldives.jpg)",
+        backgroundSize: "cover",
+        height: "100vh",
+        position: "relative",
+        width: "100%",
+      }}
+    >
+      <div className='wallpaper-content'>
+        <SearchIcon
+          sx={{
+            color: "#00B5E2",
+            borderRadius: "45%",
+            border: "1px solid white",
+            backgroundColor: "white",
+            fontSize: "1.5rem",
+            width: "4rem",
+            height: "2.5rem",
+            position: "absolute",
+            top:"20px",
+            right: "2rem",
+          }}
+        />
+        <h1> Rent a Yatch House with Yatch flex</h1>
+      </div>
+    </div>
+  </div>
+);
+
 
 export default HomePage;
