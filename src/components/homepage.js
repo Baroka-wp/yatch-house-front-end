0525f8da7129f0f60d37932b2133f5d947e65386
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-19 12:45:34 +0300

    Add dot menu to homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import SearchIcon from "@mui/icons-material/Search";
 import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
+import CircleRoundedIcon from "@mui/icons-material/CircleRounded";
 import './homepage.css';
-import { border } from '@mui/system';
+import { v4 as uuidv4 } from "uuid";
 
 
 const HomePage = () => (
@@ -17,11 +18,11 @@ const HomePage = () => (
         width: "100%",
       }}
     >
-        <div className = "hamburger">
-            <div className = "line"></div>
-            <div className = "line"></div>
-        </div>
-        <div className='wallpaper-content'>
+      <div className='hamburger'>
+        <div className='line'></div>
+        <div className='line'></div>
+      </div>
+      <div className='wallpaper-content'>
         <SearchIcon
           sx={{
             color: "#1663be",
@@ -32,17 +33,29 @@ const HomePage = () => (
             width: "2.6rem",
             height: "4rem",
             position: "absolute",
-            top:"20px",
+            top: "20px",
             right: "2rem",
-            transform:"rotate(90deg)",
+            transform: "rotate(90deg)",
           }}
         />
         <h1> Rent a Yatch House with Yatch flex</h1>
-        </div>
-        <div className="reserve-button">
-            <button className='reserve'>Reserve Now
-            <ExpandCircleDownIcon sx={{color:"#fff", transform:"rotate(-90deg)"}} />
-            </button>
+      </div>
+      <div className='reserve-button'>
+        <button className='reserve'>
+          <span>Reserve Now</span>
+          <span className='reserve-icon'>
+            <ExpandCircleDownIcon
+              sx={{ color: "#fff", transform: "rotate(-90deg)" }}
+            />
+          </span>
+        </button>
+      </div>
+        <div className='homepage-dots'>
+            {[...Array(4)].map(() => (
+                <div key={uuidv4()}>
+                    <CircleRoundedIcon sx={{ fontSize: '1rem',border: "2px solid white", borderRadius:"50%" }} />
+                </div>
+            ))}
         </div>
     </div>
   </div>
