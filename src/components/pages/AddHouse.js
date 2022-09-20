037344c5f4afbde7515eeebbf366d972add54521
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-20 06:50:25 +0300

    Create add house component

---
+++ src/components/pages/AddHouse.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HouseForm from './HouseForm';
+
+class AddHouse extends React.Component{
+    render(){
+        return(
+            <div>
+                < HouseForm />
+            </div>
+        )
+
+
+    }
+
+}
+export default AddHouse;
