50e4b4bb1c66b97b128d7d75ff7329a9ec8011f6
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-20 10:29:02 +0300

    Add form controls to the inputs

--- src/components/pages/AddHouse.js
+++ src/components/pages/AddHouse.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import HouseForm from './HouseForm';
 
-class AddHouse extends React.Component{
-    render(){
-        return(
-            <div>
-                < HouseForm />
-            </div>
-        )
-
-
-    }
-
-}
+const AddHouse = () => (
+    <div>
+        <HouseForm />
+    </div>
+);
 export default AddHouse;
--- src/components/pages/HouseForm.js
+++ src/components/pages/HouseForm.js
@@ -1,114 +1,78 @@
-import React from 'react';
+import React,{useState} from 'react';
 import axios from 'axios';
-class HouseForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      description: '',
-      price: '',
-      location: '',
-      image: '',
-    };
-  }
 
-  handleSubmit = (event) => {
-    axios({
-        method: 'post',
-        url: 'http://localhost:3000/houses',
-        data: {
-            name: this.state.name,
-            description: this.state.description,
-            price: this.state.price,
-            location: this.state.location,
-            image: this.state.image,
-        },
-        headers: {
-            'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
-        },
-    }).then((response) => {
-        console.log(response);
-    }).catch((error) => {
-        console.log(error);
+const HouseForm = () => {
+    const [inputs, setInputs] = useState({
+        name: '',
+        description: '',
+        price: '',
+        location: '',
+        image: '',
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:3000/houses', inputs)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
-    );
-    event.preventDefault();
-  }
 
-  handleChange = (event) => {
-    const name = event.target.name;
-    const newState = {};
-    newState[name] = event.target.value;
-    this.setState(newState);
-    event.preventDefault();
-  };
-
-  render() {
     return (
-      <div className="container">
-        <h1 className="text-center mt-5">Add a new house</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="description">Description</label>
-            <textarea
-              className="form-control"
-              id="description"
-              name="description"
-              value={this.state.description}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="price">Price</label>
-            <input
-              type="number"
-              className="form-control"
-              id="price"
-              name="price"
-              value={this.state.price}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="location">Location</label>
-            <input
-              type="text"
-              className="form-control"
-              id="location"
-              name="location"
-              value={this.state.location}
-              onChange={this.handleChange}
-            />
-          </div>
-            <div className="form-group">
-                <label htmlFor="image">Image</label>
-                <input
-                type="text"
-                className="form-control"
-                id="image"
-                name="image"
-                value={this.state.image}
-                onChange={this.handleChange}
-                />
-            </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='description'>Description</label>
+          <input
+            type='text'
+            className='form-control'
+            name='description'
+            id='description'
+            value={inputs.description}
+            onChange={(event) =>
+              setInputs({ ...inputs, description: event.target.value })
+            }
+          />
+          <label htmlFor='price'>Price</label>
+          <input
+            type='number'
+            className='form-control'
+            name='price'
+            id='price'
+            value={inputs.price}
+            onChange={(event) =>
+              setInputs({ ...inputs, price: event.target.value })
+            }
+          />
+          <label htmlFor='location'>Location</label>
+          <input
+            type='text'
+            className='form-control'
+            name='location'
+            id='location'
+            value={inputs.location}
+            onChange={(event) =>
+              setInputs({ ...inputs, location: event.target.value })
+            }
+          />
+          <label htmlFor='image'>Image</label>
+          <input
+            type='text'
+            className='form-control'
+            name='image'
+            id='image'
+            value={inputs.image}
+            onChange={(event) =>
+              setInputs({ ...inputs, image: event.target.value })
+            }
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    );
+};
 
-            <button type="submit" className="btn btn-primary">
-                Add House
-            </button>
-            </form>
-        </div>
-        );
-    }
-}
+
 export default HouseForm;
