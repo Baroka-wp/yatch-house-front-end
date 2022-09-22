108b32b0a23679a08a2e3b51adb006fe8bbad03c
rolandfbk <rolandfbk@gmail.com>
2022-09-22 19:21:38 +0200

    Created reservation report dummy data

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => (
         <Route path="/houses" element={<MainPage />} />
         <Route path="/houses/:id" element={<House />} />
         <Route path="/houses/:id/new" element={<AddHouse />} />
-        <Route path="/my_reservation/:id" element={<MyReservation />} />
+        <Route path="/my_reservation" element={<MyReservation />} />
         <Route path="/reservations/:id/new" element={<NewReservation />} />
         <Route path="/admins/login" element={<Login />} />
         <Route path="/admins/registration" element={<Registration />} />
--- src/components/pages/MyReservation.js
+++ src/components/pages/MyReservation.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'react-slideshow-image/dist/styles.css';
 import MobileNavbar from '../MobileNavBar';
 import SideNavbar from '../MainNavBar';
+import reservationsList from '../dummydata/reservation_report';
 
-const MyReservation = () => (
-  <div className="main_page">
-    <div className="mainNavBar">
-      <SideNavbar />
+const MyReservation = () => {
+  const [items, setItem] = useState([]);
+
+  useEffect(() => {
+    const data = reservationsList.filter((res) => res.user_id === 2);
+    setItem(data);
+  }, []);
+
+  return (
+    <div className="main_page">
+      <div className="mainNavBar">
+        <SideNavbar />
+      </div>
+      <div className="wrapper">
+        <div className="mobile_menu_background">
+          <MobileNavbar />
+        </div>
+        <div className="clearfix" />
+        <h3> Your reservation report</h3>
+        <div className="reservation_report">
+          <table>
+            <tbody>
+              {items.map((item) => (
+                <tr>
+                  <td>{item.house_name}</td>
+                  <td>{item.start_date}</td>
+                  <td>{item.end_date}</td>
+                  <td>{item.number_of_days}</td>
+                  <td>{item.total_cost}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="main_page_footer">
+          <p>© 2022 Yatch House</p>
+        </div>
+      </div>
     </div>
-    <div className="wrapper">
-      <div className="mobile_menu_background">
-        <MobileNavbar />
-      </div>
-      <div className="clearfix" />
-      <h3> Your reservation report</h3>
-      <div className="reservation_report">
-        
-      </div>
-      <div className="main_page_footer">
-        <p>© 2022 Yatch House</p>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default MyReservation;
