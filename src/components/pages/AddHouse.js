e87ca6ceb8fa42d81ee3b0623ce2568e90bffc1d
baroka <birotori@gmail.com>
2022-09-23 03:55:24 +0100

    Refactore addhouse component

--- src/components/pages/AddHouse.js
+++ src/components/pages/AddHouse.js
@@ -1,11 +1,22 @@
 import React from 'react';
 // import { Navigate } from 'react-router';
 import HouseForm from '../HouseForm';
-// import { AuthContext } from '../../context/AuthContext';
+import SideNavbar from '../MainNavBar';
+import MobileNavbar from '../MobileNavBar';
 
 const AddHouse = () => (
-  <div>
-    <HouseForm />
+  <div className="main_page">
+    <div className="mainNavBar">
+      <SideNavbar />
+    </div>
+    <div className="wrapper">
+      <div className="mobile_menu_background">
+        <MobileNavbar />
+      </div>
+      <div className="clearfix" />
+      <HouseForm />
+    </div>
+    <div className="main_page_footer"><p>© 2022 Yatch House</p></div>
   </div>
 
 );
