85ab768ee406f1213bfbfd8c3e3ca4e93c9c14eb
baroka <birotori@gmail.com>
2022-09-23 01:13:40 +0100

    Add endpoint in houseform

--- src/components/pages/AddHouse.js
+++ src/components/pages/AddHouse.js
@@ -1,15 +1,22 @@
-import React from 'react';
-// import { Redirect } from 'react-router';
+import React, { useContext, useEffect } from 'react';
+import { Navigate } from 'react-router';
 import HouseForm from '../HouseForm';
-// import { AuthContext } from '../../context/AuthContext';
+import { AuthContext } from '../../context/AuthContext';
 
-const AddHouse = () => (
-  <div>
-    <HouseForm />
-  </div>
-);
-  // const { user } = React.useContext(AuthContext);
-  // if (user.role !== 'admin') {
-  //   return <Redirect to="/" />;
-  // }
+const AddHouse = () => {
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if(!user || !user.data.admin){
+      return <Navigate to="/" />
+    }
+  }, []);
+
+  return (
+    <div>
+      <HouseForm />
+    </div>
+
+  )
+};
 export default AddHouse;
