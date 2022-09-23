ef5425b2fe4664e74877d5b306b1db411f4e5c1b
baroka <birotori@gmail.com>
2022-09-23 03:29:04 +0100

    Use useSelector to get houses frome store

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,48 +1,61 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { SplideSlide } from '@splidejs/react-splide';
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { getAllItems } from '../redux/house/houseReducer';
 import Socials from './socials';
 import '@splidejs/react-splide/css';
-import yatches from './dummydata/joydata';
+// import yatches from './dummydata/joydata';
 import './pages/mainpage.css';
 
 const Carousel = () => {
+  const yatches = useSelector((state) => state.houses);
+
   const [itemId, setItemId] = useState(0);
   const [item, setItem] = useState(yatches[itemId]);
 
-  if (itemId > yatches.length - 1) {
-    setItemId(0);
-  } else if (itemId < 0) {
-    setItemId(yatches.length - 1);
-  }
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(getAllItems());
     setItem(yatches[itemId]);
-  }, [itemId]);
+  }, [dispatch, itemId]);
+
+  if (yatches.length > 0) {
+    if (itemId > yatches.length - 1) {
+      setItemId(0);
+    } else if (itemId < 0) {
+      setItemId(yatches.length - 1);
+    }
+  }
 
   return (
     <div className="d-flex yatchCarousel">
       <button type="button" className="prev-btn" onClick={() => setItemId(itemId - 1)}>
         <ArrowLeftIcon />
       </button>
-      <SplideSlide key={item.id}>
-        <Link to={`/houses/${item.id}`} style={{ textDecoration: 'none' }}>
-          <div className="yatch">
-            <div className="card">
-              <img src={item.image} alt={item.name} />
-              <div className="yatch_name">
-                <h4>{item.name}</h4>
+      {
+        yatches.length > 0 && (
+          <SplideSlide>
+            <Link to={`/houses/${item.id}`} style={{ textDecoration: 'none' }}>
+              <div className="yatch">
+                <div className="card">
+                  <img src={item.image} alt={item.name} />
+                  <div className="yatch_name">
+                    <h4>{item.name}</h4>
+                  </div>
+                  <p>{item.description}</p>
+                  <div className="social">
+                    <Socials />
+                  </div>
+                </div>
               </div>
-              <p>{item.description}</p>
-              <div className="social">
-                <Socials />
-              </div>
-            </div>
-          </div>
-        </Link>
-      </SplideSlide>
+            </Link>
+          </SplideSlide>
+        )
+      }
       <button type="button" className="next-btn" onClick={() => setItemId(itemId + 1)}>
         <ArrowRightIcon />
       </button>
