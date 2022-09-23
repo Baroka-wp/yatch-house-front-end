a7bc8f1b368db1a5334e428c6b6df7055dd2e7a0
rolandfbk <rolandfbk@gmail.com>
2022-09-23 15:22:52 +0200

    Created DeleteHouse component

---
+++ src/components/pages/DeleteHouse.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import 'react-slideshow-image/dist/styles.css';
+import MobileNavbar from '../MobileNavBar';
+import SideNavbar from '../MainNavBar';
+import Carousel from '../carousel';
+
+const DeleteHouse = () => (
+  <div className="main_page">
+    <div className="mainNavBar">
+      <SideNavbar />
+    </div>
+    <div className="wrapper">
+      <div className="mobile_menu_background">
+        <MobileNavbar />
+      </div>
+      <div className="clearfix" />
+      <h3> Select your desired destination ....</h3>
+      <div className="carousel">
+        <Carousel />
+      </div>
+      <div className="main_page_footer">
+        <p>© 2022 Yatch House</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default DeleteHouse;
