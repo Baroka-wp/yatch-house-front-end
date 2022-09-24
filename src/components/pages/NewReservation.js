c48d0e21e86f5f571aa61c28a582f820e6e47f3b
rolandfbk <rolandfbk@gmail.com>
2022-09-24 16:42:20 +0200

    set the end date to display the date plus 1

--- src/components/pages/NewReservation.js
+++ src/components/pages/NewReservation.js
@@ -11,13 +11,14 @@ import './newreservation.css';
 import { AuthContext } from '../../context/AuthContext';
 
 const NewReservation = () => {
+  let date = new Date();
   const { user } = useContext(AuthContext);
   const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(date.setDate(date.getDate() + 1));
 
   const [reservation, setReservation] = useState({
     start_date: startDate.toISOString().split('T')[0],
-    end_date: endDate.toISOString().split('T')[0],
+    end_date: endDate,
   });
 
   const yatches = useSelector((state) => state.houses);
@@ -109,7 +110,7 @@ const NewReservation = () => {
                     <DatePicker
                       selected={endDate}
                       onChange={(date) => setEndDate(date)}
-                      minDate={new Date()}
+                      minDate={date.setDate(date.getDate() + 1)}
                     />
                   </div>
                 </div>
