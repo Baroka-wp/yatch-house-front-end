cc9ae0dcd1836dc5b416f8f1e1add501b42fd2c7
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-26 17:26:38 +0300

    Add AddHouse test

--- src/test/AddHouse.test.js
+++ src/test/AddHouse.test.js
@@ -1,3 +1,22 @@
 import AddHouse from '../components/pages/AddHouse';
 import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
+import store from '../redux/store';
+import { BrowserRouter } from 'react-router-dom';
+
+
+
+describe('addhouse', () => {
+    it('renders addhouse component', () => {
+        const component = render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <AddHouse />
+                </BrowserRouter>
+            </Provider>,
+        );
+        expect(component).toMatchSnapshot();
+    });
+
+});
 
