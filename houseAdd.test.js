50d3f6fa1f739a8bc0435cc6aa922fdb5dd66cb9
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-26 17:40:37 +0200

    Add houseadd mock function

---
+++ houseAdd.test.js
@@ -0,0 +1,12 @@
+import addHouse from './__mocks__/houseAdd';
+
+describe('addHouse', () => {
+    it('should add a house', () => {
+        const myhouses = [];
+        const houseName = 'house1';
+        const location = 'location1';
+        const price = 100;
+        const result = addHouse(houseName, location, price, myhouses);
+        expect(result).toEqual([{name:houseName, location:location, price:price}]);
+    });
+    });
