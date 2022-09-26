0a0da6ae1610387f17b34ad474619c9d12e2f13d
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-26 20:59:25 +0200

    Refactor house deletion mock function

--- src/test/HouseDeletion.test.js
+++ src/test/HouseDeletion.test.js
@@ -1,21 +1,60 @@
 import HouseDelete from '../../__mocks__/deleteHouse';
 
 describe('HouseDelete', () => {
-    it('should delete a house', () => {
-        const myhouses = [{ id: 1, name: 'house1', location: 'location1', price: 100 }];
-        const house_id = 1;
-        const result = HouseDelete(house_id, myhouses);
-        expect(result).toEqual([]);
-    }
-    );
+  it('should delete a house', () => {
+    const myhouses = [{
+      id: 1, name: 'house1', location: 'location1', price: 100,
+    }];
+    const houseId = 1;
+    const reservations = [
+      {
+        id: 1, houseId: 1, name: 'house1', location: 'location1', price: 100,
+      },
+      {
+        id: 2, houseId: 2, name: 'house2', location: 'location2', price: 200,
+      }];
+    const result = HouseDelete(houseId, myhouses, reservations);
+    expect(result.houses).toEqual([]);
+  });
 
-    it('should delete a specific house', () => {
-        let myhouses = [
-            { id: 1, name: 'house1', location: 'location1', price: 100 },
-            { id: 2, name: 'house2', location: 'location2', price: 200 }];
-        const house_id = 1;
-        const result = HouseDelete(house_id, myhouses);
-        expect(result.length).toEqual(1);
-    }
-    );
+  it('should delete a specific house', () => {
+    const myhouses = [
+      {
+        id: 1, name: 'house1', location: 'location1', price: 100,
+      },
+      {
+        id: 2, name: 'house2', location: 'location2', price: 200,
+      }];
+    const houseId = 1;
+    const reservations = [
+      {
+        id: 1, houseId: 1, name: 'house1', location: 'location1', price: 100,
+      },
+      {
+        id: 2, houseId: 2, name: 'house2', location: 'location2', price: 200,
+      }];
+    const result = HouseDelete(houseId, myhouses, reservations);
+    expect(result.houses.length).toEqual(1);
+  });
+
+  it('should delete a all related reservations ', () => {
+    const myhouses = [
+      {
+        id: 1, name: 'house1', location: 'location1', price: 100,
+      },
+      {
+        id: 2, name: 'house2', location: 'location2', price: 200,
+      }];
+    const houseId = 1;
+
+    const reservations = [
+      {
+        id: 1, houseId: 1, name: 'house1', location: 'location1', price: 100,
+      },
+      {
+        id: 2, houseId: 2, name: 'house2', location: 'location2', price: 200,
+      }];
+    const result = HouseDelete(houseId, myhouses, reservations);
+    expect(result.reservations.length).toEqual(1);
+  });
 });
