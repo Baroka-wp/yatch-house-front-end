fe720d1a93eac43b2c179510a5c2f536ef247be1
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-26 18:27:53 +0200

    Create second test for addhouse functionality

--- src/test/houseAdd.test.js
+++ src/test/houseAdd.test.js
@@ -10,7 +10,14 @@ describe('addHouse', () => {
         expect(result).toEqual([{name:houseName, location:location, price:price}]);
     });
 
+    it('should add a house to others', () => {
+        const myhouses = [];
+        const houseName = 'house1';
+        const location = 'location1';
+        const price = 100;
+        const result1 = addHouse(houseName, location, price, myhouses);
+        const result2 = addHouse(houseName, location, price, result1);
+        expect(result2.length).toEqual(2);
+    });
 
-
-    
     });
