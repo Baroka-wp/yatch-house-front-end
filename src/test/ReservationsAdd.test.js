80aaa284d93bee4e62015c8d9289a4f280eed09d
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-26 18:21:19 +0200

    Create second reservation add test

--- src/test/ReservationsAdd.test.js
+++ src/test/ReservationsAdd.test.js
@@ -24,8 +24,34 @@ describe('addHouse', () => {
                 houseName: 'house1',
                 location: 'location1',
                 price: 100,
-                
+
             },
         ]);
+        const secondReservation = newReservation(0, 1, 2, newReservations, houses);
+
+        expect(secondReservation.length).toEqual(2);
+    });
+
+    it('should add keep sevaral reservations', () => {
+        const houses = [
+            {
+                name: 'house1',
+                location: 'location1',
+                price: 100,
+            },
+            {
+                name: 'house2',
+                location: 'location2',
+                price: 200,
+            },
+        ];
+
+        const reservations = [];
+
+        const newReservations = newReservation(0, 1, 2, reservations, houses);
+
+        const secondReservation = newReservation(0, 1, 2, newReservations, houses);
+
+        expect(secondReservation.length).toEqual(2);
     });
 });
