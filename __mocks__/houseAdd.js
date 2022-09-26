3ac0113073b2d3302cf5d9bea00a56256f103fe7
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-26 17:51:35 +0200

    Move houseAdd to test

--- __mocks__/houseAdd.js
+++ __mocks__/houseAdd.js
@@ -1,3 +1,5 @@
 export default function newHouse(houseName, location, price, myhouses){
-    myhouses.push({name:houseName, location:location, price:price})
+    myhouses.push({ name: houseName, location: location, price: price })
+    
+    return myhouses
 }
