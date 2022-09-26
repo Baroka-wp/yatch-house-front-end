dbe8304db90fd86fd08f61c4ff216d58978c95a8
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-26 20:59:02 +0200

    Refactor house creation mock function

--- src/test/houseAdd.test.js
+++ src/test/houseAdd.test.js
@@ -1,23 +1,22 @@
 import addHouse from '../../__mocks__/houseAdd';
 
 describe('addHouse', () => {
-    it('should add a house', () => {
-        const myhouses = [];
-        const houseName = 'house1';
-        const location = 'location1';
-        const price = 100;
-        const result = addHouse(houseName, location, price, myhouses);
-        expect(result).toEqual([{name:houseName, location:location, price:price}]);
-    });
+  it('should add a house', () => {
+    const myhouses = [];
+    const houseName = 'house1';
+    const location = 'location1';
+    const price = 100;
+    const result = addHouse(houseName, location, price, myhouses);
+    expect(result).toEqual([{ name: houseName, location, price }]);
+  });
 
-    it('should add a house to others', () => {
-        const myhouses = [];
-        const houseName = 'house1';
-        const location = 'location1';
-        const price = 100;
-        const result1 = addHouse(houseName, location, price, myhouses);
-        const result2 = addHouse(houseName, location, price, result1);
-        expect(result2.length).toEqual(2);
-    });
-
-    });
+  it('should add a house to others', () => {
+    const myhouses = [];
+    const houseName = 'house1';
+    const location = 'location1';
+    const price = 100;
+    const result1 = addHouse(houseName, location, price, myhouses);
+    const result2 = addHouse(houseName, location, price, result1);
+    expect(result2.length).toEqual(2);
+  });
+});
