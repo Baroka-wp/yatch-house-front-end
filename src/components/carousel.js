45b7179a480fe8b3e5359bf618addb8a3c87d581
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-27 14:55:13 +0200

    Add Carouselcard to the carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -13,6 +13,7 @@ import { getAllItems } from '../redux/house/houseReducer';
 import Socials from './socials';
 import '@splidejs/react-splide/css';
 import './pages/mainpage.css';
+import CarrouselCard from './CarrouselCard';
 
 const Carousel = () => {
   const yatches = useSelector((state) => state.houses);
@@ -87,61 +88,15 @@ const Carousel = () => {
   };
 
   return (
-    <div className="d-flex yatchCarousel">
-      {
-       handlePrev()
-      }
-      {item ? (
-        <SplideSlide>
-          <Link to={`/houses/${item.id}`} style={{ textDecoration: 'none' }}>
-            <div className="yatch">
-              <div className="card">
-                <img src={item.image} alt={item.name} />
-                <div className="yatch_name">
-                  <h4>{item.name}</h4>
-                </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    gap: '20px',
-                    alignItems: 'center',
-                    marginBottom: '0',
-                  }}
-                  className="yatch_location"
-                >
-                  <FontAwesomeIcon
-                    icon={faLocationDot}
-                    style={{ width: '25px', height: '10px', color: '#97bf0f' }}
-                  />
-                  <span
-                    style={{
-                      fontSize: '1rem',
-                      fontFamily: 'Niconne, cursive',
-                      marginLeft: '-20px',
-                    }}
-                  >
-                    {item.location}
-                  </span>
-                </div>
-                <p>{item.description}</p>
-                <div className="social">
-                  <Socials />
-                </div>
-              </div>
-            </div>
-          </Link>
-        </SplideSlide>
-      ) : (
-        <Stack spacing={1}>
-          {/* For variant="text", adjust the height via font-size */}
-          <Skeleton variant="rectangular" width={310} height={250} />
-          <Skeleton variant="rounded" width={310} height={100} />
-          <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
-        </Stack>
-      )}
-      {handleNext()}
-    </div>
+    <CarrouselCard
+    item={item}
+    handlePrev={handlePrev}
+    handleNext={handleNext}
+    handleDeleteHouse={'handleDeleteHouse'}
+    isLoading={'isLoading'}
+    admin={false}
+  />
+     
   );
 };
 
