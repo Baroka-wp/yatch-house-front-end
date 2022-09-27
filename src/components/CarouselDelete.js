e5ac43c9a5cfd5b51fd27232557eb27ac3aba83d
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-27 14:55:37 +0200

    Add CarouselCard to carouselDelete

--- src/components/CarouselDelete.js
+++ src/components/CarouselDelete.js
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import axios from 'axios';
-import { SplideSlide } from '@splidejs/react-splide';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import Skeleton from '@mui/material/Skeleton';
-import Stack from '@mui/material/Stack';
 import { deleteHouse, getAllItems } from '../redux/house/houseReducer';
-import Socials from './socials';
 import '@splidejs/react-splide/css';
 import './pages/mainpage.css';
+import CarrouselCard from './CarrouselCard'
 
 const CarouselDelete = () => {
   const yatches = useSelector((state) => state.houses, shallowEqual);
-
   const [itemId, setItemId] = useState(0);
   const [item, setItem] = useState(yatches[itemId]);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -38,11 +31,8 @@ const CarouselDelete = () => {
   }
 
   const [deleteNotif, setNotif] = useState('');
-
   const [isLoading, setShow] = useState(false);
-
   const [canDisplay, diplayNotif] = useState(false);
-
   const timeOut = () => {
     const timer = setTimeout(() => {
       setNotif('');
@@ -148,73 +138,14 @@ const CarouselDelete = () => {
         )}
       </div>
 
-      <div className="d-flex yatchCarousel">
-        {
-          handlePrev()
-        }
-        {
-          item ? (
-            <SplideSlide>
-              <div className="yatch">
-                <div className="card">
-                  <img src={item.image} alt={item.name} />
-                  <div className="yatch_name">
-                    <h4>{item.name}</h4>
-                  </div>
-                  <div
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      gap: '20px',
-                      alignItems: 'center',
-                      marginBottom: '0',
-                    }}
-                    className="yatch_location"
-                  >
-                    <FontAwesomeIcon
-                      icon={faLocationDot}
-                      style={{ width: '25px', height: '10px', color: '#97bf0f' }}
-                    />
-                    <span
-                      style={{
-                        fontSize: '1rem',
-                        fontFamily: 'Niconne, cursive',
-                        marginLeft: '-20px',
-                      }}
-                    >
-                      {item.location}
-                    </span>
-                  </div>
-                  <p>{item.description}</p>
-                  <div className="social">
-                    <Socials />
-                  </div>
-                </div>
-              </div>
-              <button
-                onClick={() => handleDeleteHouse(item.id)}
-                disabled={isLoading}
-                type="button"
-                className="btn btn-danger"
-                style={{ marginTop: '20px' }}
-              >
-                {isLoading ? 'Deleting...' : 'Delete this house'}
-              </button>
-            </SplideSlide>
-          ) : (
-            <Stack spacing={1}>
-              {/* For variant="text", adjust the height via font-size */}
-              <Skeleton variant="rectangular" width={310} height={250} />
-              <Skeleton variant="rounded" width={310} height={100} />
-              <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
-            </Stack>
-          )
-        }
-        {
-          handleNext()
-        }
-
-      </div>
+      <CarrouselCard
+        item={item}
+        handlePrev={handlePrev}
+        handleNext={handleNext}
+        handleDeleteHouse={handleDeleteHouse}
+        isLoading={isLoading}
+        admin={true}
+      />
     </>
   );
 };
